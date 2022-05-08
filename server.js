dea503637a8ed5c5dc8bcd1d8403e573360d4fc7
Erik Woidt <erik@woidt.be>
2022-05-08 13:47:55 +0200

    fade in & out for slides + backup slide & buffer

--- server.js
+++ server.js
@@ -1,29 +1,42 @@
 const express = require('express')
 const app = express()
 const engine = require('express-handlebars').engine
+
 const port = 3000
+const slides_url = '/slides';
+const backup_url = '/default.html'
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+app.use(express.static('public'))
 
-var CachingProxy=require('caching-proxy')
-var proxy = new CachingProxy({
-    port: 9090, 
-    dir: './data/cached-data'
+//TODO replace with real code (first isolate backend code)
+app.get('/slide/:id', (req, res) => {
+    if((0 + req.params.id) > 5) { // only respond to part of the possible slides
+        res.status(404);
+        return res.send()
+    }
+    res.send('Test hello ' + req.params.id);
 })
+
+//TODO replace with real code
+app.get('/slides', (req, res) => {
+    const slides = [];
+    for(var i = 0; i < 10; i++){
+        slides.push({url: '/slide/' + i, sleep: 5000 })
+    }
+    res.json(slides)
+})
+
 
 app.get('/', (req, res) => {
     res.render('main',{
-        id: 1
+        slides_url: slides_url, // fetch list of slide + sleep info
+        backup_slide: backup_url // link to local url with general backup slide
     });
 })
 
-// called via cache server
-app.get('/slide/:id', (req, res) => {
-    res.send('Hello World!' + req.params.id)
-})
-
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`listening on port ${port}`)
 })
